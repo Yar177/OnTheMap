b7bdcde265c713b9caae4dc230f2f0a8ba9b3f2c
Yar177 <yarsher.dev@gmail.com>
2019-08-07 15:45:49 -0400

    pin done

--- OnTheMap/Controllers/ViewController.swift
+++ OnTheMap/Controllers/ViewController.swift
@@ -47,15 +47,18 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
         let reuseId = "pin"
         
         var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
-        if pinView == nil{
+        
+        if pinView == nil {
             pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
             pinView!.canShowCallout = true
             pinView!.pinTintColor = .red
             pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }else{
+        }
+        else {
             pinView!.annotation = annotation
         }
         
@@ -64,14 +67,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        if control == view.rightCalloutAccessoryView{
+        if control == view.rightCalloutAccessoryView {
             let app = UIApplication.shared
             if let toOpen = view.annotation?.subtitle! {
-                app.openURL(URL(string: toOpen)!)
+                app.open(URL(string: toOpen)!)
             }
         }
     }
-
+  
     
     
     
