35a4103622fc8b910175fe8d64f42d8f30412956
Yar177 <yarsher.dev@gmail.com>
2019-08-08 00:14:43 -0400

    login ui

---
+++ OnTheMap/Controllers/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Hoshiar Sher on 8/7/19.
+//  Copyright © 2019 Hoshiar Sher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func login(_ sender: Any) {
+        
+    }
+    
+    
+}
